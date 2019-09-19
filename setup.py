ea8b41a3b03846ecce67ee39e48c86b4136c41a5
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-09-19 09:11:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,28 +7,28 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
   
 setup(
-  name = 'secure-graphene',         # How you named your package folder (MyLib)
-  packages = ['secure_graphene'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Python library that assists you in securing your GraphQL API against malicious queries',   # Give a short description about your library
+  name = 'secure-graphene',         
+  packages = ['secure_graphene'],  
+  version = '0.1',      
+  license='MIT',        
+  description = 'Python library that assists you in securing your GraphQL API against malicious queries',   
   long_description=read('README.md'),
   long_description_content_type='text/markdown',
-  author = 'Zachary Manesiotis',                   # Type in your name
-  author_email = 'zack.manesiotis@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/manesioz/secure-graphene',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['graphql', 'python', 'security', 'graphene'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  author = 'Zachary Manesiotis',        
+  author_email = 'zack.manesiotis@gmail.com',    
+  url = 'https://github.com/manesioz/secure-graphene',   
+  download_url = 'https://github.com/manesioz/secure-graphene/archive/v1.0.tar.gz',    
+  keywords = ['graphql', 'python', 'security', 'graphene', 'api'],   
+  install_requires=[
           'graphql-core',
           'graphene',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
